a5de56ab57e09e7a5ac8d6d0feb944a96e7ec5be
17wh1a0570 <17wh1a0570@bvrithyderabad.edu.in>
2020-03-09 23:21:22 +0530

    Created class Builder design

---
+++ DesignPatterns/src/main/java/com/epam/DesignPatterns/BuilderDesign.java
@@ -0,0 +1,64 @@
+package com.epam.DesignPatterns;
+
+class Patternvehicle {
+ 
+  private String engine;
+  private int wheel;
+  
+  private int airbags;
+  
+  public String getEngine() {
+    return this.engine;
+  }
+  
+  public int getWheel() {
+    return this.wheel;
+  }
+  
+  public int getAirbags() {
+    return this.airbags;
+  }
+  
+  private Patternvehicle(Vehicle builder) {
+    this.engine = builder.engine;
+    this.wheel = builder.wheel;
+    this.airbags = builder.airbags;
+  }
+  
+  public static class Vehicle {
+    private String engine;
+    private int wheel;
+    
+    private int airbags;
+    
+    public Vehicle(String engine, int wheel) {
+      this.engine = engine;
+      this.wheel = wheel;
+    }
+    
+    public Vehicle setAirbags(int airbags) {
+      this.airbags = airbags;
+      return this;
+    }
+    
+    public Patternvehicle build() {
+      return new Patternvehicle(this);
+    }
+  }
+}
+
+public class BuilderDesign {
+  
+  public static void main(String[] args) {
+    Patternvehicle car = new Patternvehicle.Vehicle("1500cc", 4).setAirbags(4).build();
+  Patternvehicle bike = new Patternvehicle.Vehicle("250cc", 2).build();
+    
+    System.out.println(car.getEngine());
+    System.out.println(car.getWheel());
+    System.out.println(car.getAirbags());
+    
+    System.out.println(bike.getEngine());
+    System.out.println(bike.getWheel());
+    System.out.println(bike.getAirbags());
+  }
+}
