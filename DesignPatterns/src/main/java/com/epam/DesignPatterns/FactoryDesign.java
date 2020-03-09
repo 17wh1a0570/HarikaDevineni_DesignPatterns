8483a9a624964f553e865bb92bc9154df5a6d9d2
17wh1a0570 <17wh1a0570@bvrithyderabad.edu.in>
2020-03-09 23:21:36 +0530

    Added class implementing factory design

---
+++ DesignPatterns/src/main/java/com/epam/DesignPatterns/FactoryDesign.java
@@ -0,0 +1,59 @@
+package com.epam.DesignPatterns;
+
+abstract class Vehicle {
+  public abstract int getWheel();
+  
+  public String toString() {
+    return "Wheel: " + this.getWheel();
+  }
+}
+
+class Car extends Vehicle {
+  int wheel;
+  
+  Car(int wheel) {
+    this.wheel = wheel;
+  }
+
+  @Override
+  public int getWheel() {
+    return this.wheel;
+  }
+}
+
+class Bike extends Vehicle {
+  int wheel;
+  
+  Bike(int wheel) {
+    this.wheel = wheel;
+  }
+  
+  @Override
+  public int getWheel() {
+    return this.wheel;
+  }
+}
+
+class VehiclesFactory {
+  public static Vehicle getInstance(String type, int wheel) {
+    if(type == "car") {
+      return new Car(wheel);
+    } else if(type == "bike") {
+      return new Bike(wheel);
+    }
+    
+    return null;
+  }
+}
+
+public class FactoryDesign {
+
+  public static void main(String[] args) {
+    Vehicle car = VehiclesFactory.getInstance("car", 4);
+    System.out.println(car);
+    
+    Vehicle bike = VehiclesFactory.getInstance("bike", 2);
+    System.out.println(bike);
+  }
+
+}
